0dd0a60dddd3ba5b3e75caa18962928ed7193abd
fbuch2 <fbuchtorrents@gmail.com>
2024-01-12 16:07:21 +0100

    New class for filtering

--- scripts/rep_first_script.py
+++ scripts/rep_first_script.py
@@ -5,14 +5,32 @@ Script to make updates in github
 import pandas as pd
 import click
 
-click.command(short_help='Parser to import datset')
-click.option('-f', '--file_name', required=True, help='File to import')
+class FilteringClass:
+    """
+    Class for filtering
+    """
+
+    def __init__(self,df):
+        self.df = df
+
+    def filter_price(self, price):
+        """
+        Filter price
+        """
+        return self.df[self.df['Price Starting With ($)'] < price]
+
+@click.command(short_help='Parser to import datset')
+@click.option('-f', '--file_name', required=True, help='File to import')
 def main(file_name):
     """
     Main function
     """
     df = pd.read_csv(file_name)
-    print(df.shape)
+    
+    
+    result = FilteringClass(df).filter_price(12)
+    
+    print(result.shape)
 
 if __name__ == "__main__":
     main()

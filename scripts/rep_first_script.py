2f418dc2232307560b216e4343e22dd511a01fc1
fbuch2 <fbuchtorrents@gmail.com>
2024-01-12 15:43:04 +0100

    Import dataset with click

--- scripts/rep_first_script.py
+++ scripts/rep_first_script.py
@@ -3,12 +3,16 @@ Script to make updates in github
 """
 
 import pandas as pd
+import click
 
-def main():
+click.command(short_help='Parser to import datset')
+click.option('-f', '--file_name', required=True, help='File to import')
+def main(file_name):
     """
     Main function
     """
-    pass
+    df = pd.read_csv(file_name)
+    print(df.shape)
 
 if __name__ == "__main__":
     main()
